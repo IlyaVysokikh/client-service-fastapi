d6cccbb52b2ca71321c5d5758a2d864cbff2a3ef
IlyaVysokikh <vysokikh.ilya03@gmail.com>
2024-12-02 02:06:25 +0500

    fix module level import

--- src/web/main.py
+++ src/web/main.py
@@ -1,11 +1,4 @@
-import os
-
 from fastapi import FastAPI
-
-print("Путь к файлу main.py:", os.path.abspath(__file__))
-print("Текущий рабочий каталог:", os.getcwd())
-
-
 from src.web.user_handlers import user_router
 
 
